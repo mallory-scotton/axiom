be4ab152e2179918ae92ab2a405215c31140142b
mallory-scotton <mallory.scotton@epitech.eu>
2024-10-16 15:23:23 +0200

    [ADD] Adding 3D vector to the math module

---
+++ include/axiom/math/vector3.hpp
@@ -0,0 +1,103 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// MIT License
+///
+/// Copyright(c) 2024 Mallory SCOTTON
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following coditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software?
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Pragma once
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace ax::math
+///////////////////////////////////////////////////////////////////////////////
+namespace ax::math
+{
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+///////////////////////////////////////////////////////////////////////////////
+struct Vector3
+{
+public:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    union
+    {
+        struct
+        {
+            float x;        //<!
+            float y;        //<!
+            float z;        //<!
+        };
+        float xyz[3];       //<!
+    };
+
+public:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    static const Vector3 zeroVector;        //<!
+    static const Vector3 oneVector;         //<!
+    static const Vector3 upVector;          //<!
+    static const Vector3 downVector;        //<!
+    static const Vector3 forwardVector;     //<!
+    static const Vector3 backwardVector;    //<!
+    static const Vector3 leftVector;        //<!
+    static const Vector3 rightVector;       //<!
+    static const Vector3 xAxisVector;       //<!
+    static const Vector3 yAxisVector;       //<!
+    static const Vector3 zAxisVector;       //<!
+
+public:
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    Vector3(void);
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    /// \param f
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    Vector3(float f);
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief
+    ///
+    /// \param x
+    /// \param y
+    /// \param z
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    Vector3(float x, float y, float z);
+};
+
+} // namespace ax::math
